ea12572d37c1b90467cd408d925998e9cd9446d8
Daniil Nababkin <43863412+TheRavehorn@users.noreply.github.com>
2020-08-23 00:28:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
 except ModuleNotFoundError:
     subprocess.call("apt-get update", shell=True)
     subprocess.call("apt-get install python3-pip git tcpdump libnfnetlink-dev libnetfilter-queue-dev -y", shell=True)
-    subprocess.call("pip3 install python3-scapy --quiet", shell=True)
+    subprocess.call("pip3 install scapy --quiet", shell=True)
     subprocess.call("pip3 install -U git+https://github.com/kti/python-netfilterqueue --quiet", shell=True)
 finally:
     subprocess.call("clear", shell=True)
